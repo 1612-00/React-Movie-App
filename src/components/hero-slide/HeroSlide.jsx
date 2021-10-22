3b6fb27668a711b8a74316df8f72700e95eb06b2
1612-00 <nguyenducanh.ldb@gmail.com>
2021-10-22 22:24:19 +0700

    Done

--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -23,7 +23,7 @@ const HeroSlide = () => {
         const response = await tmdbApi.getMoviesList(movieType.popular, {
           params,
         });
-        setMovieItems(response.results.slice(7, 10));
+        setMovieItems(response.results.slice(7, 11));
       } catch (error) {
         console.log(error);
       }
